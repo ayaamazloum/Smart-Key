87e1c46ecc5c48aa1b5b651999773f6407410f6c
ayaamazloum <ayamazloum.a@gmail.com>
2024-04-28 18:42:46 +0300

    Adding email input

--- smart_key/lib/screens/login_screen.dart
+++ smart_key/lib/screens/login_screen.dart
@@ -9,6 +9,7 @@ class LoginScreen extends StatelessWidget {
       backgroundColor: Theme.of(context).primaryColor,
       body: Container(
         margin: EdgeInsets.only(top: 180.0),
+        padding: EdgeInsets.symmetric(vertical: 0, horizontal: 25),
         decoration: BoxDecoration(
           borderRadius: BorderRadiusDirectional.only(
               topStart: Radius.circular(40), topEnd: Radius.circular(40)),
@@ -20,7 +21,7 @@ class LoginScreen extends StatelessWidget {
             Container(
               width: 100,
               height: 100,
-              padding: EdgeInsets.all(15),
+              padding: EdgeInsets.all(20),
               transform: Matrix4.translationValues(0, -40, 0),
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -40,6 +41,29 @@ class LoginScreen extends StatelessWidget {
             Text(
               'Log In',
               style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            SizedBox(
+              height: 55,
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(left: 20),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  labelText: 'Email',
+                  hintText: 'example@example.com',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade400,
+                    fontWeight: FontWeight.w300,
+                    ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
             ),
           ],
         ),
