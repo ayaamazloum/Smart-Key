25f5efe963b969e08a8e0e8f642877eb32cdf270
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-06 16:24:17 +0300

    Arduino: Adding open and close door buttons functionality

--- arduino/system/system.ino
+++ arduino/system/system.ino
@@ -7,20 +7,23 @@ const char* password = "81713264";
 
 const char* mqtt_server = "test.mosquitto.org";
 const int mqtt_port = 1883;
-const char* door_topic_control = "arduino1door/control";
-// const char* knock_topic_control = "arduino1knock/setup";
-// const char* passcode_topic_control = "arduino1passcode/setup";
+const char* door_control_topic = "arduino1door/control";
+// const char* knock_control_topic = "arduino1knock/setup";
+// const char* passcode_control_topic = "arduino1passcode/setup";
 
-const char* door_topic_status = "arduino1door/status";
+const char* door_status_topic = "arduino1door/status";
 
 WiFiClient espClient;
 PubSubClient client(espClient); 
 
 Servo servo;
 const int servoPin = D4;
+#define flashLed D0
 #define lock D1
 #define bell D2
 #define bellButton D3
+#define openDoorButton D5
+#define closeDoorButton D6
 
 void setup() {
   Serial.begin(115200);
@@ -30,9 +33,12 @@ void setup() {
   client.setCallback(callback);
   
   servo.attach(servoPin);
+  pinMode(flashLed, OUTPUT);
   pinMode(lock, OUTPUT);
   pinMode(bell, OUTPUT);
   pinMode(bellButton, INPUT_PULLUP);
+  pinMode(openDoorButton, INPUT_PULLUP);
+  pinMode(closeDoorButton, INPUT_PULLUP);
   
   closeDoor();
 }
@@ -46,6 +52,14 @@ void loop() {
   if (digitalRead(bellButton) == LOW) {
     ringBell();
   }
+
+  if (digitalRead(openDoorButton) == LOW) {
+    openDoor();
+  }
+
+  if (digitalRead(closeDoorButton) == LOW) {
+    closeDoor();
+  }
 }
 
 void ringBell() {
@@ -55,7 +69,7 @@ void ringBell() {
 void openDoor() {
   digitalWrite(lock, HIGH);
   servo.write(180);
-  digitalWrite(D4, HIGH);
+  digitalWrite(flashLed, LOW);
   publishDoorStatus("opened");
   Serial.println("Door opened");
   tone(bell, 2500, 400);
@@ -66,14 +80,14 @@ void openDoor() {
 void closeDoor() {
   digitalWrite(lock, LOW);
   servo.write(0);
-  digitalWrite(D4, LOW);
+  digitalWrite(flashLed, HIGH);
   publishDoorStatus("closed");
   Serial.println("Door closed");
   tone(bell, 200, 400);
 }
 
 void publishDoorStatus(String doorStatus) {
-  if (client.publish(door_topic_status, doorStatus.c_str())) {
+  if (client.publish(door_status_topic, doorStatus.c_str())) {
     Serial.println("Publish door status successful!");
   } else {
     Serial.println("Publish door status failed :(");
@@ -86,7 +100,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
     message += (char)payload[i];
   }
   
-  if (strcmp(topic, door_topic_control) == 0) {
+  if (strcmp(topic, door_control_topic) == 0) {
     if (message.equals("open")) {
       openDoor();
     } else if (message.equals("close")) {
@@ -100,7 +114,7 @@ void reconnect() {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("SmartKeyESP8266Client")) {
       Serial.println("connected");
-      client.subscribe(door_topic_control);
+      client.subscribe(door_control_topic);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
