da80c07f36eaa46694cef1354d4ff605d60c8557
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-07 14:27:17 +0300

    Backend: Cleaning unwanted logic

--- laravel_server/app/Http/Middleware/Authenticate.php
+++ laravel_server/app/Http/Middleware/Authenticate.php
@@ -9,32 +9,6 @@ use Carbon\Carbon;
 
 class Authenticate extends Middleware
 {
-    public function handle($request, Closure $next)
-    {
-        if (!Auth::check()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-        
-        $user = Auth::user();
-
-        if (!$this->isValidUser($user)) {
-            Auth::logout();
-            
-            $user->delete();
-
-            return response()->json(['error' => 'Invitation expired.'], 401);
-        }
-
-        return $next($request);
-    }
-
-    protected function isValidUser($user)
-    {
-        $now = Carbon::now();
-
-        return $user->end_date >= $now;
-    }
-
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
--- laravel_server/app/Http/Middleware/CheckRole.php
+++ laravel_server/app/Http/Middleware/CheckRole.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class CheckRole
 {
@@ -13,14 +14,13 @@ class CheckRole
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $role): Response
     {
         $user = Auth::user();
 
         if ($user && $user->role->role === $role) {
             return $next($request);
         }
-
         return response()->json(['error' => 'Unauthorized access.'], 403);
     }
 }
