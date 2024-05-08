71759ba6dd1fac6b125717f3826da7cd76519b0b
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-08 17:58:16 +0300

    Frontend: Adding all settings items in settings screen

--- smart_key/lib/screens/settings_screen.dart
+++ smart_key/lib/screens/settings_screen.dart
@@ -43,36 +43,73 @@ class SettingsScreenState extends State<SettingsScreen> {
                 fit: BoxFit.contain,
               ),
             ),
+            Positioned(
+              top: screenHeight(context) * 0.04,
+              left: (screenWidth(context) - screenWidth(context) * 0.35) / 2,
+              child: Text('Settings',
+                            style: Theme.of(context).textTheme.headlineLarge)
+            ),
             Container(
               padding: EdgeInsets.only(
                   left: screenWidth(context) * 0.05,
-                  right: screenWidth(context) * 0.05,
-                  top: screenHeight(context) * 0.04),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Align(
-                      alignment: Alignment.center,
-                      child: Text('Settings',
-                          style: Theme.of(context).textTheme.headlineLarge)),
-                  SizedBox(height: screenHeight(context) * 0.04),
-                  SingleChildScrollView(
-                      child: Column(children: [
-                    Text('General',
-                        style: Theme.of(context).textTheme.bodyLarge),
-                  ])),
-                  SizedBox(height: screenHeight(context) * 0.015),
-                  SettingsItem(icon: Icons.group_add_outlined, title: 'Invitations'),
-                  SettingsItem(icon: Icons.sensor_door_outlined, title: 'Knock Pattern'),
-                  SettingsItem(icon: Icons.lock_open_outlined, title: 'Change Passcode'),
-                  SizedBox(height: screenHeight(context) * 0.03),
-                  PrimaryButton(
-                      text: 'Logout',
-                      onPressed: () {
-                        preferences.clear();
-                        Navigator.pushReplacementNamed(context, '/login');
-                      }),
-                ],
+                  right: screenWidth(context) * 0.05,),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: screenHeight(context) * 0.1),
+                    Column(children: [
+                      Text('General',
+                          style: Theme.of(context).textTheme.bodyMedium),
+                      SizedBox(height: screenHeight(context) * 0.002),
+                      SettingsItem(
+                          icon: Icons.group_add_outlined,
+                          title: 'Invitations',
+                          route: ''),
+                      SettingsItem(
+                          icon: Icons.sensor_door_outlined,
+                          title: 'Knock pattern',
+                          route: ''),
+                      SettingsItem(
+                          icon: Icons.lock_open_outlined,
+                          title: 'Change passcode',
+                          route: ''),
+                      SizedBox(height: screenHeight(context) * 0.02),
+                      SingleChildScrollView(
+                          child: Column(children: [
+                        Text('Account',
+                            style: Theme.of(context).textTheme.bodyMedium)
+                      ])),
+                      SizedBox(height: screenHeight(context) * 0.002),
+                      SettingsItem(
+                          icon: Icons.group_add_outlined,
+                          title: 'Profile',
+                          route: ''),
+                      SettingsItem(
+                          icon: Icons.lock_open_outlined,
+                          title: 'Change password',
+                          route: ''),
+                      SizedBox(height: screenHeight(context) * 0.02),
+                      SingleChildScrollView(
+                          child: Column(children: [
+                        Text('App',
+                            style: Theme.of(context).textTheme.bodyMedium)
+                      ])),
+                      SizedBox(height: screenHeight(context) * 0.002),
+                      SettingsItem(
+                          icon: Icons.group_add_outlined,
+                          title: 'Privacy policy',
+                          route: ''),
+                      SizedBox(height: screenHeight(context) * 0.02),
+                      PrimaryButton(
+                          text: 'Logout',
+                          onPressed: () {
+                            preferences.clear();
+                            Navigator.pushReplacementNamed(context, '/login');
+                          }),
+                    ]),
+                  ],
+                ),
               ),
             ),
           ],
--- smart_key/lib/widgets/settings_item.dart
+++ smart_key/lib/widgets/settings_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class SettingsItem extends StatelessWidget {
-  const SettingsItem({super.key, required this.icon, required this.title});
+  const SettingsItem({super.key, required this.icon, required this.title, required this.route});
 
   final IconData icon;
   final String title;
+  final String route;
 
   @override
   Widget build(BuildContext context) {
@@ -12,25 +13,31 @@ class SettingsItem extends StatelessWidget {
       padding: EdgeInsets.all(10),
       margin: EdgeInsets.only(bottom: 10),
       decoration: BoxDecoration(
-        border: Border(bottom: BorderSide(color: Colors.grey.shade300, width: 1)),
+        border:
+            Border(bottom: BorderSide(color: Colors.grey.shade300, width: 1)),
       ),
-      child: Row(
-        children: [
-          Expanded(
-              flex: 1,
-              child: Icon(
-                icon,
-                color: Colors.grey.shade600,
-              )),
-          Expanded(child: Container()),
-          Expanded(
-              flex: 18,
-              child:
-                  Text(title, style: Theme.of(context).textTheme.bodyMedium)),
-          Expanded(
-              flex: 1,
-              child: Icon(Icons.arrow_forward, color: Colors.grey.shade600)),
-        ],
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed(route);
+        },
+        child: Row(
+          children: [
+            Expanded(
+                flex: 1,
+                child: Icon(
+                  icon,
+                  color: Colors.grey.shade600,
+                )),
+            Expanded(child: Container()),
+            Expanded(
+                flex: 18,
+                child:
+                    Text(title, style: Theme.of(context).textTheme.bodySmall)),
+            Expanded(
+                flex: 1,
+                child: Icon(Icons.arrow_forward, color: Colors.grey.shade600)),
+          ],
+        ),
       ),
     );
   }
