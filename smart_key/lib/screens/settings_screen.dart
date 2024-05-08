ace6c86797ee78cb76341e7962a4468c118518cb
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-08 18:37:07 +0300

    Frontend: Finalizing settings screen design

--- smart_key/lib/screens/settings_screen.dart
+++ smart_key/lib/screens/settings_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smart_key/utils/constants.dart';
-import 'package:smart_key/widgets/primary_button.dart';
 import 'package:smart_key/widgets/settings_item.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -44,65 +43,85 @@ class SettingsScreenState extends State<SettingsScreen> {
               ),
             ),
             Positioned(
-              top: screenHeight(context) * 0.04,
-              left: (screenWidth(context) - screenWidth(context) * 0.35) / 2,
-              child: Text('Settings',
-                            style: Theme.of(context).textTheme.headlineLarge)
-            ),
+                top: screenHeight(context) * 0.04,
+                left: (screenWidth(context) - screenWidth(context) * 0.35) / 2,
+                child: Text('Settings',
+                    style: Theme.of(context).textTheme.headlineLarge)),
             Container(
               padding: EdgeInsets.only(
-                  left: screenWidth(context) * 0.05,
-                  right: screenWidth(context) * 0.05,),
+                left: screenWidth(context) * 0.05,
+                right: screenWidth(context) * 0.05,
+              ),
               child: SingleChildScrollView(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     SizedBox(height: screenHeight(context) * 0.1),
-                    Column(crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                      Text('General',
-                          style: Theme.of(context).textTheme.bodyMedium),
-                      SizedBox(height: screenHeight(context) * 0.01),
-                      SettingsItem(
-                          icon: Icons.group_add_outlined,
-                          title: 'Invitations',
-                          route: ''),
-                      SettingsItem(
-                          icon: Icons.sensor_door_outlined,
-                          title: 'Knock pattern',
-                          route: ''),
-                      SettingsItem(
-                          icon: Icons.lock_open_outlined,
-                          title: 'Change passcode',
-                          route: ''),
-                      SizedBox(height: screenHeight(context) * 0.02),
-                        Text('Account',
-                            style: Theme.of(context).textTheme.bodyMedium),
-                      SizedBox(height: screenHeight(context) * 0.01),
-                      SettingsItem(
-                          icon: Icons.person_outline_sharp,
-                          title: 'Profile',
-                          route: ''),
-                      SettingsItem(
-                          icon: Icons.password_outlined,
-                          title: 'Change password',
-                          route: ''),
-                      SizedBox(height: screenHeight(context) * 0.02),
-                        Text('App',
-                            style: Theme.of(context).textTheme.bodyMedium),
-                      SizedBox(height: screenHeight(context) * 0.01),
-                      SettingsItem(
-                          icon: Icons.privacy_tip_outlined,
-                          title: 'Privacy policy',
-                          route: ''),
-                      SizedBox(height: screenHeight(context) * 0.02),
-                      PrimaryButton(
-                          text: 'Logout',
-                          onPressed: () {
-                            preferences.clear();
-                            Navigator.pushReplacementNamed(context, '/login');
-                          }),
-                    ]),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('General',
+                              style: Theme.of(context).textTheme.bodyMedium),
+                          SizedBox(height: screenHeight(context) * 0.01),
+                          SettingsItem(
+                              icon: Icons.group_add_outlined,
+                              title: 'Invitations',
+                              route: ''),
+                          SettingsItem(
+                              icon: Icons.sensor_door_outlined,
+                              title: 'Knock pattern',
+                              route: ''),
+                          SettingsItem(
+                              icon: Icons.lock_open_outlined,
+                              title: 'Change passcode',
+                              route: ''),
+                          SizedBox(height: screenHeight(context) * 0.02),
+                          Text('Account',
+                              style: Theme.of(context).textTheme.bodyMedium),
+                          SizedBox(height: screenHeight(context) * 0.01),
+                          SettingsItem(
+                              icon: Icons.person_outline_sharp,
+                              title: 'Profile',
+                              route: ''),
+                          SettingsItem(
+                              icon: Icons.password_outlined,
+                              title: 'Change password',
+                              route: ''),
+                          SizedBox(height: screenHeight(context) * 0.02),
+                          Text('App',
+                              style: Theme.of(context).textTheme.bodyMedium),
+                          SizedBox(height: screenHeight(context) * 0.01),
+                          SettingsItem(
+                              icon: Icons.privacy_tip_outlined,
+                              title: 'Privacy policy',
+                              route: ''),
+                          SizedBox(height: screenHeight(context) * 0.03),
+                          GestureDetector(
+                            onTap: () {
+                              preferences.clear();
+                              Navigator.pushReplacementNamed(context, '/login');
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.logout_rounded),
+                                SizedBox(width: screenWidth(context) * 0.02),
+                                Text(
+                                  'Log Out',
+                                  style: Theme.of(context).textTheme.bodyMedium,
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: screenHeight(context) * 0.04),
+                          Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Version 0.0.1\nAll rights reserved.',
+                              style: TextStyle(color: Colors.grey, fontSize: 12), textAlign: TextAlign.center,
+                            ),
+                          )
+                        ]),
                   ],
                 ),
               ),
