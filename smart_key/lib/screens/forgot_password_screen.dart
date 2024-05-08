40641a8c8f0ee3d01141d1c933e890a7afe37436
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-08 20:32:41 +0300

    Frontend: Adding forgot password widgets

--- smart_key/lib/screens/forgot_password_screen.dart
+++ smart_key/lib/screens/forgot_password_screen.dart
@@ -1,15 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:smart_key/utils/constants.dart';
+import 'package:smart_key/utils/input_methods.dart';
+import 'package:smart_key/widgets/primary_button.dart';
+import 'package:smart_key/widgets/text_field.dart';
+import 'package:logger/logger.dart';
 
-class ForgotPassword extends StatefulWidget {
-  const ForgotPassword({super.key});
+class ForgotPassword extends StatelessWidget {
+  ForgotPassword({super.key});
 
-  @override
-  State<ForgotPassword> createState() => ForgotPasswordState();
-}
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
 
-class ForgotPasswordState extends State<ForgotPassword> {
+  final logger = Logger();
+
+  void forgotPassword(BuildContext context) {}
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                'assets/images/screen_shape_1.png',
+                width: screenWidth(context) * 0.35,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Positioned(
+                top: screenHeight(context) * 0.04,
+                left: (screenWidth(context) - screenWidth(context) * 0.53) / 2,
+                child: Text('Forgot Password',
+                    style: Theme.of(context).textTheme.headlineLarge)),
+            Container(
+              padding: EdgeInsets.only(
+                left: screenWidth(context) * 0.05,
+                right: screenWidth(context) * 0.05,
+              ),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: screenHeight(context) * 0.15),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Enter your email address to send you a password reset key.',
+                          style: Theme.of(context).textTheme.bodySmall,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(height: screenHeight(context) * 0.06),
+                      MyTextField(
+                        labelText: 'E-mail',
+                        hintText: 'example@example.com',
+                        controller: emailController,
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              !value.isValidEmail) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                        obscureText: false,
+                        textInputType: TextInputType.emailAddress,
+                      ),
+                      SizedBox(height: screenHeight(context) * 0.04),
+                      PrimaryButton(
+                        text: 'Submit',
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            forgotPassword(context);
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
