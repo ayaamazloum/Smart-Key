0859a87b946e0646b6207a07de1bfc40c84d30f6
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-10 01:25:17 +0300

    Backend: Creating add device api

--- laravel_server/app/Http/Controllers/NotificationController.php
+++ laravel_server/app/Http/Controllers/NotificationController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Firebase\Messaging\Notification;
+use App\Models\Device;
 
 class NotificationController extends Controller
 {
@@ -13,7 +14,7 @@ class NotificationController extends Controller
     {
         $title = 'Visitor Notification';
         $body = 'Someone is at your door, ringing the bell';
-        $token = '';
+        $token = 'euDMrQEGTAyIAXeMmhlyLB:APA91bHxWRbNyfdBev5WxQ1U2x97AWKzwMub6L782pVk_asYkr9m7NAN3hJXp60SwwSSrMtw9ySQ4AHOKfiDXtRNoUoU4XHkeem0HGIhL9V4TWyok3TNdCejsgzMvgy2zctB1g6mpJFI';
 
         $firebase = (new Factory())
         ->withServiceAccount(__DIR__.'/../../../config/firebase_credentials.json');
@@ -26,5 +27,20 @@ class NotificationController extends Controller
         $messaging->send($message);
 
         return response()->json(['message' => 'Notification sent successfully']);
+    }
+
+    public function addDevice(Request $request) {
+        $request->validate([
+            'token' =>'required|string',
+        ]);
+
+        Device::create([
+            'fcm_token' => $request->token,
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Device added successfully.',
+        ]);
     }
 }
