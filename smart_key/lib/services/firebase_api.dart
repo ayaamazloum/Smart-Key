474b737d82b012cc357ae15c255b07a0304fc7a1
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-09 19:29:55 +0300

    Frontend: Handling message on background test

--- smart_key/lib/services/firebase_api.dart
+++ smart_key/lib/services/firebase_api.dart
@@ -1,9 +1,22 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:logger/logger.dart';
+
+Logger logger = Logger();
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  logger.i('Title ${message.notification?.title}');
+  logger.i('Body ${message.notification?.body}');
+  logger.i('Payload ${message.data}');
+}
 
 class FirebaseApi {
   final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
 
   Future<void> initNotifications() async {
     await firebaseMessaging.requestPermission();
+    final fCMToken = await firebaseMessaging.getToken();
+    logger.i('token: $fCMToken');
+
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage());
   }
 }
