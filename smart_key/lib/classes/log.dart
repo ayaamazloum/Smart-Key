082afd93eb4146e38d051a93b782e0d86c2e02af
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-10 17:54:02 +0300

    Frontend: Adding log class into classes folder

---
+++ smart_key/lib/classes/log.dart
@@ -0,0 +1,19 @@
+class Log {
+  final int id;
+  final String log;
+  final String date;
+
+  Log({
+    required this.id,
+    required this.log,
+    required this.date,
+  });
+
+  factory Log.fromJson(Map<String, dynamic> json) {
+    return Log(
+      id: json['id'],
+      log: json['log'],
+      date: DateTime.parse(json['created_at']).toString().split(" ")[0],
+    );
+  }
+}
