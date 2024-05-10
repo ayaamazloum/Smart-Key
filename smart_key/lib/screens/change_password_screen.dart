d8fcd718e4ae68304ab02aaa5a7f45a81b74d053
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-11 00:08:01 +0300

    Frontend: Adding password inputs to change password screen

--- smart_key/lib/screens/change_password_screen.dart
+++ smart_key/lib/screens/change_password_screen.dart
@@ -1,9 +1,20 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:smart_key/services/api.dart';
 import 'package:smart_key/utils/constants.dart';
+import 'package:smart_key/utils/input_methods.dart';
+import 'package:smart_key/widgets/primary_button.dart';
+import 'package:smart_key/widgets/text_field.dart';
 import 'package:logger/logger.dart';
 
 class ChangePasswordScreen extends StatelessWidget {
   ChangePasswordScreen({super.key});
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController currentPasswordController = TextEditingController();
+  final TextEditingController newPasswordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
 
   final logger = Logger();
 
@@ -42,7 +53,85 @@ class ChangePasswordScreen extends StatelessWidget {
                 left: screenWidth(context) * 0.05,
                 right: screenWidth(context) * 0.05,
               ),
-              child: SingleChildScrollView(),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: screenHeight(context) * 0.15),
+                      Text(
+                        'We have sent you a verification code on your email address.',
+                        style: Theme.of(context).textTheme.bodySmall,
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: screenHeight(context) * 0.06),
+                      MyTextField(
+                        labelText: 'Current Password',
+                        hintText: '********',
+                        controller: currentPasswordController,
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              !value.isValidPassword) {
+                            return 'Password must be at least 8 characters long';
+                          }
+                          return null;
+                        },
+                        showVisibilityIcon: true,
+                        obscureText: true,
+                        textInputType: TextInputType.emailAddress,
+                      ),
+                      SizedBox(
+                        height: screenHeight(context) * 0.025,
+                      ),
+                      MyTextField(
+                        labelText: 'New Password',
+                        hintText: '********',
+                        controller: newPasswordController,
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              !value.isValidPassword) {
+                            return 'Password must be at least 8 characters long';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        textInputType: TextInputType.visiblePassword,
+                      ),
+                      SizedBox(
+                        height: screenHeight(context) * 0.025,
+                      ),
+                      MyTextField(
+                        labelText: 'Confirm Password',
+                        hintText: '********',
+                        controller: confirmPasswordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please confirm your password';
+                          }
+                          if (value != newPasswordController.text) {
+                            return 'Passwords don\'t match';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        textInputType: TextInputType.visiblePassword,
+                      ),
+                      SizedBox(
+                        height: screenHeight(context) * 0.05,
+                      ),
+                      PrimaryButton(
+                        text: 'Reset Password',
+                        onPressed: () {
+                          
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             )
           ],
         ),
