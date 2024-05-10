d53132c0ed61294d7339da6e4c99a80a73dc7147
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-11 00:06:11 +0300

    Frontend: Creatinf change password screen initial ui

---
+++ smart_key/lib/screens/change_password_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:smart_key/utils/constants.dart';
+import 'package:logger/logger.dart';
+
+class ChangePasswordScreen extends StatelessWidget {
+  ChangePasswordScreen({super.key});
+
+  final logger = Logger();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                'assets/images/screen_shape_1.png',
+                width: screenWidth(context) * 0.35,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Positioned(
+              top: screenHeight(context) * 0.04,
+              left: 10,
+              child: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Positioned(
+                top: screenHeight(context) * 0.04,
+                left: (screenWidth(context) - screenWidth(context) * 0.53) / 2,
+                child: Text('Forgot Password',
+                    style: Theme.of(context).textTheme.headlineLarge)),
+            Container(
+              padding: EdgeInsets.only(
+                left: screenWidth(context) * 0.05,
+                right: screenWidth(context) * 0.05,
+              ),
+              child: SingleChildScrollView(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
