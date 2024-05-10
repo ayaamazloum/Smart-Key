0f5cd5453da7b8d5441ccc01d05f70c29df8e9f9
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-10 17:54:37 +0300

    Frontend: Creating invitation class

---
+++ smart_key/lib/classes/invitation.dart
@@ -0,0 +1,11 @@
+class Invitation {
+  final int id;
+  final String email;
+  final String type;
+
+  Invitation({
+    required this.id,
+    required this.email,
+    required this.type,
+  });
+}
