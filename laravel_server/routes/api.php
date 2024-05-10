6098ea641889c5c9c6803d62521cbaa0da8dd320
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-10 22:01:06 +0300

    Backend: Adding delete invitation api route

--- laravel_server/routes/api.php
+++ laravel_server/routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth')->group(function () {
             Route::post('invite', [InvitationController::class, 'sendInvitation']);
             Route::post('logs', [HomeController::class, 'getLogs']);
             Route::get('invitations', [InvitationController::class, 'getAllInvitations']);
+            Route::post('deleteInvitation', [InvitationController::class, 'deleteInvitation']);
         });
 
         Route::middleware('role:family_member,guest')->group(function () {
