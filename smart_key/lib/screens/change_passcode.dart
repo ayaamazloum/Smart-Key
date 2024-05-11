886181fba809ae41def1c61646aa94977530d02a
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-11 13:34:00 +0300

    Frontend: Adding passcode inputs in change passcode screen

--- smart_key/lib/screens/change_passcode.dart
+++ smart_key/lib/screens/change_passcode.dart
@@ -1,9 +1,20 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:smart_key/services/api.dart';
 import 'package:smart_key/utils/constants.dart';
+import 'package:smart_key/utils/input_methods.dart';
+import 'package:smart_key/widgets/primary_button.dart';
+import 'package:smart_key/widgets/text_field.dart';
 import 'package:logger/logger.dart';
 
 class ChangePasscodeScreen extends StatelessWidget {
   ChangePasscodeScreen({super.key});
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController currentPasscodeController = TextEditingController();
+  final TextEditingController newPasscodeController = TextEditingController();
+  final TextEditingController confirmPasscodeController =
+      TextEditingController();
 
   final logger = Logger();
 
@@ -37,7 +48,111 @@ class ChangePasscodeScreen extends StatelessWidget {
                 left: (screenWidth(context) - screenWidth(context) * 0.53) / 2,
                 child: Text('Change Passcode',
                     style: Theme.of(context).textTheme.headlineLarge)),
-            ],
+            Container(
+              padding: EdgeInsets.only(
+                left: screenWidth(context) * 0.05,
+                right: screenWidth(context) * 0.05,
+              ),
+              margin: EdgeInsets.only(top: screenHeight(context) * 0.17),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Passcode must is at least 8 characters long and can contain only digits and uppercase letters A-D.',
+                        style: Theme.of(context).textTheme.bodySmall,
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: screenHeight(context) * 0.06),
+                      MyTextField(
+                        labelText: 'Current Passcode',
+                        hintText: '********',
+                        controller: currentPasscodeController,
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              !value.isValidPasscode) {
+                            return 'Invalid passcode format';
+                          }
+                          return null;
+                        },
+                        showVisibilityIcon: true,
+                        obscureText: true,
+                        textInputType: TextInputType.emailAddress,
+                      ),
+                      SizedBox(
+                        height: screenHeight(context) * 0.025,
+                      ),
+                      MyTextField(
+                        labelText: 'New Passcode',
+                        hintText: '********',
+                        controller: newPasscodeController,
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              !value.isValidPasscode) {
+                            return 'Invalid passcode format';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        textInputType: TextInputType.visiblePassword,
+                      ),
+                      SizedBox(
+                        height: screenHeight(context) * 0.025,
+                      ),
+                      MyTextField(
+                        labelText: 'Confirm Passcode',
+                        hintText: '********',
+                        controller: confirmPasscodeController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please confirm your Passcode';
+                          }
+                          if (value != newPasscodeController.text) {
+                            return 'Passcodes don\'t match';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        textInputType: TextInputType.visiblePassword,
+                      ),
+                      SizedBox(
+                        height: screenHeight(context) * 0.05,
+                      ),
+                      PrimaryButton(
+                        text: 'Change Passcode',
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: screenHeight(context) * 0.015,
+                      ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(formKey.currentContext!).pushNamed('/forgotPasscode');
+                        },
+                        child: Text(
+                          'forgot Passcode?',
+                          style: TextStyle(
+                            color: primaryColor,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
