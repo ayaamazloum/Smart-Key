a39416d309b5103d01a9ab818520c5107f50d771
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-11 18:54:21 +0300

    Frontend: Creating knock screen

---
+++ smart_key/lib/screens/knock_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KncockScreen extends StatelessWidget {
+  const KncockScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
