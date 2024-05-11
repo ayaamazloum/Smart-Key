e58b6a7c013bb9ea06e85ffc90750ff55ecd1283
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-11 18:06:29 +0300

    Backend: Creating update profile api

--- laravel_server/app/Http/Controllers/UserController.php
+++ laravel_server/app/Http/Controllers/UserController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function updateProfile(Request $request)
+    {
+        $user = auth()->user();
+
+        if ($request->has('newProfilePicture')) {
+            $imageData = $request->input('newProfilePicture');
+            $imageName = 'profile_' . $user->id . '_' . uniqid() . '.png';
+            $imagePath = 'profile_pictures/' . $imageName;
+            Storage::disk('public')->put($imagePath, base64_decode($imageData));
+
+            $user->profile_picture = $imageName;
+        }
+
+        if ($request->has('name')) {
+            $user->name = $request->input('name');
+        }
+
+        $user->save();
+
+        return response()->json([
+            'status' => 'success', 
+            'message' => 'Profile updated successfully',
+            'profilePicture' => $imageName
+        ]);
+    }
 }
