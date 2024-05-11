75cefa83ab49017fd85babe3ab5991baf7c42535
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-11 15:58:30 +0300

    Frontend: Adding route for profile picture screen

--- smart_key/lib/main.dart
+++ smart_key/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:smart_key/screens/forgot_password_screen.dart';
 import 'package:smart_key/screens/invitations_screen.dart';
 import 'package:smart_key/screens/members_at_home_screen.dart';
 import 'package:smart_key/screens/notifications_screen.dart';
+import 'package:smart_key/screens/profile_screen.dart';
 import 'package:smart_key/screens/reset_password_screen.dart';
 import 'package:smart_key/services/firebase_api.dart';
 import 'package:smart_key/utils/constants.dart';
@@ -124,6 +125,7 @@ class SmartKeyState extends State<SmartKey> {
         '/invitations': (context) => InvitationsScreen(),
         '/changePassword': (context) => ChangePasswordScreen(),
         '/changePasscode': (context) => ChangePasscodeScreen(),
+        '/profile': (context) => ProfileScreen(),
       },
       navigatorKey: navigatorKey,
     );
