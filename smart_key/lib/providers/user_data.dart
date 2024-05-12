e375617e18288e2c4e4bcd5adb9eef83d918b4ac
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-12 22:09:18 +0300

    Frontend: Creating user data class for the provider

---
+++ smart_key/lib/providers/user_data.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UserData extends ChangeNotifier {
+  String _name = '';
+
+  String get name => _name;
+
+  void setName(String newName) {
+    _name = newName;
+    notifyListeners();
+  }
+}
