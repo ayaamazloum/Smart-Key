234c99c9bf2b18a62131bbfea22d85c3c54ef381
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-12 16:08:41 +0300

    Backend: Sending notifications for token taken from devices table

--- laravel_server/app/Http/Controllers/NotificationController.php
+++ laravel_server/app/Http/Controllers/NotificationController.php
@@ -14,19 +14,22 @@ class NotificationController extends Controller
     {
         $title = 'Visitor Notification';
         $body = 'Someone is at your door, ringing the bell';
-        $token = 'euDMrQEGTAyIAXeMmhlyLB:APA91bHxWRbNyfdBev5WxQ1U2x97AWKzwMub6L782pVk_asYkr9m7NAN3hJXp60SwwSSrMtw9ySQ4AHOKfiDXtRNoUoU4XHkeem0HGIhL9V4TWyok3TNdCejsgzMvgy2zctB1g6mpJFI';
+
+        $tokens = Device::all()->pluck('fcm_token')->toArray();
 
         $firebase = (new Factory())
         ->withServiceAccount(__DIR__.'/../../../config/firebase_credentials.json');
 
         $messaging = $firebase->createMessaging();
 
-        $message = CloudMessage::withTarget('token', $token)
-            ->withNotification(Notification::create($title, $body));
+        foreach ($tokens as $token) {
+            $message = CloudMessage::withTarget('token', $token)
+                ->withNotification(Notification::create($title, $body));
+        
+            $messaging->send($message);
+        }
 
-        $messaging->send($message);
-
-        return response()->json(['message' => 'Notification sent successfully']);
+        return response()->json(['message' => 'Notifications sent successfully']);
     }
 
     public function addDevice(Request $request) {
