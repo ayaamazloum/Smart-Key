5c1b924119fd9213164a962d810e00fedc85ff61
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-13 21:59:05 +0300

    Frontend: Changing some constants values

--- smart_key/lib/utils/constants.dart
+++ smart_key/lib/utils/constants.dart
@@ -6,12 +6,13 @@ Color secondaryColor = HexColor('#373737');
 Color tertiaryColor = HexColor('#FEDF57');
 Color buttonPressColor = HexColor('#265849');
 
-String apiUrl='http://192.168.0.107:8000/api';
-String serverImagesUrl='http://192.168.0.107:8000/profile_pictures';
+String apiUrl = 'http://192.168.1.5:8000/api';
+String serverImagesUrl = 'http://192.168.1.5:8000/profile_pictures';
 
-double screenHeight (BuildContext context) {
+double screenHeight(BuildContext context) {
   return MediaQuery.of(context).size.height;
 }
-double screenWidth (BuildContext context) {
+
+double screenWidth(BuildContext context) {
   return MediaQuery.of(context).size.width;
 }
