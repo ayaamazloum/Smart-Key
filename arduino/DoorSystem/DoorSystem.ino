1d2b88d72e7ca1c81c631ac939b27c8fc6966e58
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-14 22:42:27 +0300

    Arduino: Adding a function that counts ones in a string

--- arduino/DoorSystem/DoorSystem.ino
+++ arduino/DoorSystem/DoorSystem.ino
@@ -144,6 +144,16 @@ void checkKnock() {
   }
 }
 
+int countOnes(String str) {
+  int count = 0;
+  for (int i = 0; i < str.length(); i++) {
+    if (str.charAt(i) == '1') {
+      count++;
+    }
+  }
+  return count;
+}
+
 void sendNotification() {
   WiFiClient client;
   HTTPClient http;
