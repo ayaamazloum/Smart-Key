5617692749d26518d5528fe2ca43c56ee780f370
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-18 11:48:37 +0300

    Backend: Updating user factory

--- laravel_server/database/factories/UserFactory.php
+++ laravel_server/database/factories/UserFactory.php
@@ -18,11 +18,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
+            'arduino_id' => 1,
+            'role_id' => 1,
         ];
     }
 
