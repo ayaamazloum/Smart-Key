f63b635f9048de84445edb5e5465fb7abe35cb2c
ayaamazloum <ayamazloum.a@gmail.com>
2024-05-18 14:14:45 +0300

    Backend: Creating invitation factory

---
+++ laravel_server/database/factories/InvitationFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invitation>
+ */
+class InvitationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'email' => $this->faker->unique()->safeEmail,
+            'type' => $this->faker->randomElement(['owner', 'family_member', 'guest']),
+            'key' => $this->faker->unique()->bothify('??##??##'),
+            'arduino_id' => 1,
+            'start_date' => $this->faker->dateTimeBetween('now', '+1 month'),
+            'end_date' => $this->faker->dateTimeBetween('+1 month', '+2 months'),
+        ];
+
+    }
+}
